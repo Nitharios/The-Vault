12991f8e6e61c53ca12b54f6c18378b5301a1825
Nathan N <Nitharios@users.noreply.github.com>
2017-09-13 15:09:19 -1000

    All tests passing

--- vault.js
+++ vault.js
@@ -1,4 +1,23 @@
 'use strict';
 module.exports = function() {
-  
+  var mySecret = {};
+
+  return {
+    setValue: setValue,
+    getValue: getValue
+  };
+
+  function setValue( key, value ) {
+    mySecret[key] = value;
+  }
+
+  function getValue( key ) {
+    if (key === undefined || !mySecret.hasOwnProperty(key)) {
+      return null;
+
+    } else if (mySecret.hasOwnProperty(key)) {
+      return mySecret[key];
+      //mySecret.key
+    }
+  }
 };
